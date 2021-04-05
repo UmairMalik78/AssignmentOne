ccbddba39965e78396564f9865fe740af3905c29
UmairMalik78 <bsef18m013@pucit.edu.pk>
2021-04-06 02:06:27 +0500

    Data retrieving successful

--- AssignmentOne/app/src/main/java/com/umairmaik/assignmentone/QuizActivity.java
+++ AssignmentOne/app/src/main/java/com/umairmaik/assignmentone/QuizActivity.java
@@ -40,7 +40,7 @@ public class QuizActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
-
+		//Used to get full screen view
 		this.getWindow().getDecorView().setSystemUiVisibility(
 				View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
